b06c14bec24566aef0fa7d6b93c17b8ae4def49c
Serhii Buria <sergeiburya@gmail.com>
2022-06-22 13:43:24 +0300

    next solution

--- src/main/java/mate/academy/PasswordValidator.java
+++ src/main/java/mate/academy/PasswordValidator.java
@@ -4,11 +4,12 @@ public class PasswordValidator {
     public void validate(String password, String repeatPassword) throws Exception {
         UserService userService = new UserService();
         if (password != null && repeatPassword != null && password != "" && repeatPassword != "") {
+
         } else {
             throw new Exception("Your passwords are incorrect. Try again.");
         }
-        if ((password.equals(repeatPassword))
-                && ((password == repeatPassword))) {
+        if ((password.equals(repeatPassword)) && ((password == repeatPassword))) {
+
         } else {
             throw new PasswordValidationException("Wrong passwords");
         }

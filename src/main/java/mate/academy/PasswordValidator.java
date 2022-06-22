680ad72c573fd2193b5a66b9296c8636d72eafa1
Serhii Buria <sergeiburya@gmail.com>
2022-06-22 16:10:45 +0300

    the final solution of the problemÑÑ

--- src/main/java/mate/academy/PasswordValidator.java
+++ src/main/java/mate/academy/PasswordValidator.java
@@ -7,9 +7,14 @@ public class PasswordValidator {
 
         boolean checkPassNull = password != null && password != "";
         boolean equalityTwoPass = password == repeatPassword || password.equals(repeatPassword);
-        boolean lengthPass = password.length() >= 10 && repeatPassword.length() >= 10;
+        boolean lengthPass = false;
+        if (checkPassNull) {
+            lengthPass = password.length() >= 10 && repeatPassword.length() >= 10;
+        }
 
-        if (!checkPassNull || !lengthPass || !equalityTwoPass) {
+        if (checkPassNull && equalityTwoPass && lengthPass) {
+            return;
+        } else {
             throw new PasswordValidationException("Wrong passwords");
         }
     }

4cb4167674ffca6a035cbf96848d76a391c5558d
Serhii Buria <sergeiburya@gmail.com>
2022-06-22 12:33:42 +0300

    password verification is transferred to the validate method

--- src/main/java/mate/academy/PasswordValidator.java
+++ src/main/java/mate/academy/PasswordValidator.java
@@ -1,11 +1,24 @@
 package mate.academy;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password.length() >= 10 & repeatPassword.length() >= 10) {
+    public void validate(String password, String repeatPassword) throws Exception {
+
+        UserService userService = new UserService();
+
+        if (password != null && repeatPassword != null && password != "" && repeatPassword != "") {
+
+        } else {
+            throw new Exception("Your passwords are incorrect. Try again.");
+        }
+
+
+        if ((password.length() >= 10 && repeatPassword.length() >= 10)
+                && (password.equals(repeatPassword))
+                && ((password == repeatPassword))) {
             return;
         } else {
             throw new PasswordValidationException("Wrong passwords");
         }
+
     }
 }
--- src/main/java/mate/academy/UserService.java
+++ src/main/java/mate/academy/UserService.java
@@ -4,23 +4,12 @@ public class UserService {
   public void registerUser(User user) {
     PasswordValidator validation = new PasswordValidator();
     UserService userService = new UserService();
-    boolean checkValidation = false;
+
     try {
-      if (user.getPassword() != null & user.getRepeatPassword() != null
-              & user.getPassword() != "" & user.getRepeatPassword() != "") {
         validation.validate(user.getPassword(), user.getRepeatPassword());
-        checkValidation = true;
-      } else {
-        throw new Exception ("Your passwords are incorrect. Try again.");
-      }
-       if (checkValidation == true & ((user.getPassword() == user.getRepeatPassword())
-              || (user.getPassword().equals(user.getRepeatPassword())))) {
         userService.saveUser(new User(user.getEmail(), user.getPassword(), user.getRepeatPassword()));
-      } else {
-        throw new PasswordValidationException("Wrong passwords");
-      }
     } catch (PasswordValidationException ex) {
-        System.out.println("Your passwords are incorrect. Try again.");
+        System.out.println("Wrong passwords");
     } catch (Exception e) {
       System.out.println("Your passwords are incorrect. Try again.");
     }
